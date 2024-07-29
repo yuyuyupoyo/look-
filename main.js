6e15e66749a77127da2536c79a3250e04452a6a2
yuyuyu <y.imai@iimon.co.jp>
2024-07-29 17:52:21 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let imgOne = document.getElementsByClassName("imgOne")[0];
+let imgTwo = document.getElementsByClassName("imgOne")[1];
+imgTwo.style.display = "none";
+
+function createImage(picOne, picTwo) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      picOne.style.display = "none";
+      picTwo.style.display = "block";
+      resolve();
+    }, 2000);
+  });
+}
+
+async function processImages(imgOne, imgTwo) {
+  try {
+    await createImage(imgOne, imgTwo);
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        imgTwo.style.display = "none";
+        resolve();
+      }, 2000);
+    });
+  } catch (error) {
+    console.log("出直しな");
+  } finally {
+    console.log("おつかれさま");
+  }
+}
+
+// この関数を呼び出す例
+processImages(imgOne, imgTwo);
